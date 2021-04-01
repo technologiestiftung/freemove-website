e3ad48af85ee252220ffa9fa0f1961c3f842b5d6
Lucas Soeth <lucasoeth@gmail.com>
2021-04-01 11:33:16 +0200

    style: changed font to Saira SemiCondensed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addWatchTarget("/src/assets/css/style.compiled.css");
   eleventyConfig.addPassthroughCopy({ "src/assets/common": "." });
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ["Helvetica", "sans-serif"],
+      sans: ["Saira SemiCondensed", "sans-serif"],
     },
     extend: {
       colors: {

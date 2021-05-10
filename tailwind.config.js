262333be99597efdc5e0b4155282a040913e40d6
Lucas Soeth <lucasoeth@gmail.com>
2021-05-10 18:59:02 +0200

    feat: tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,18 +10,18 @@ module.exports = {
     },
     extend: {
       colors: {
-        blue: '#293377',
-        orange: '#EA5B0C',
-        green: '#00983A',
-        purple: '#662483',
+        blue: "#293377",
+        orange: "#EA5B0C",
+        green: "#00983A",
+        purple: "#662483",
       },
     },
   },
   variants: {
     extend: {
-      scale: ['active'],
-      translate: ['active'],
+      scale: ["active"],
+      translate: ["active"],
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };

459251ab8c262de120d4767d3fcda2217ab3834b
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-01-06 17:00:05 +0100

    feat(FREEMOVE-292): apply changes
    - include YT video
    - include audio recording
    - include links for speakers
    - other minor things

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/css");
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("src/assets/audio");
   eleventyConfig.addWatchTarget("/src/assets/css/style.compiled.css");
   eleventyConfig.addPassthroughCopy({ "src/assets/common": "." });
 

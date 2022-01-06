f903fd618a4d54beb9069f7ec0887f700a738836
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-01-06 11:53:16 +0100

    feat(FREEMOVE-292): add collection for events by year

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,27 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addWatchTarget("/src/assets/css/style.compiled.css");
   eleventyConfig.addPassthroughCopy({ "src/assets/common": "." });
-  
+
+  eleventyConfig.addCollection("events_by_year", function (collectionApi) {
+    const eventItems = collectionApi.getFilteredByTag("event");
+    const uniqueEventYears = [
+      ...new Set(
+        eventItems.map((item) => {
+          return new Date(item.data.date).getFullYear();
+        })
+      ),
+    ];
+    const eventsByYear = uniqueEventYears.map((year) => {
+      return {
+        year: year,
+        events: eventItems.filter((event) => {
+          return new Date(event.data.date).getFullYear() === year;
+        }),
+      };
+    });
+    return eventsByYear;
+  });
+
   return {
     dir: {
       data: "_data",
